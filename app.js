59cd16acb16082c769564eb87a8d74fd58d1cd02
coreypnorris <coreypnorris@gmail.com>
2022-03-26 13:21:23 -0700

    Add index.html file.

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-// init Express
+// setup constants
 const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 const app = express();
 
 // setup middleware
 // app.use(morgan('combined')); // use this to see maximum information in the logs.
 app.use(morgan('tiny')); // use this to see minimal information in the logs.
+app.use(express.static(path.join(__dirname, '/public')));
 
 // setup routing
 app.get('/', (req, res) => {
